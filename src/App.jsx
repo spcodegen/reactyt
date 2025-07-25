9b700829c6af34575bb4ecdb3c42efa0e93a85e4
spcodegen <shehansp2000@gmail.com>
2025-07-25 12:29:39 +0530

    Understanding Reducer Using Bank Account Example

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,34 @@ import { useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 0 });
+  const [state, dispatch] = useReducer(reducer, { balance: 0 });
+  const [amount, setAmount] = useState(0);
+
   function reducer(state, action) {
-    if (action.type == "increment") {
-      return { ...state, count: state.count + state.incrementBy };
+    if (action.type == "add") {
+      return { ...state, balance: state.balance + action.payload };
     }
-    if (action.type == "decrement") {
-      return { ...state, count: state.count - state.incrementBy };
-    }
-    if (action.type == "setIncrement") {
-      return { ...state, incrementBy: action.payload };
+    if (action.type == "sub") {
+      return { ...state, balance: state.balance - action.payload };
     }
   }
   return (
     <>
-      <h2>{state.count}</h2>
+      <h1>{state.balance}</h1>
       <input
-        value={state.incrementBy}
-        onChange={(e) =>
-          dispatch({ type: "setIncrement", payload: Number(e.target.value) })
-        }
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
       />
-      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
-        Increment
+      <button
+        onClick={() => dispatch({ type: "add", payload: Number(amount) })}
+      >
+        Deposit
       </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
-        Decrement
+      <button
+        onClick={() => dispatch({ type: "sub", payload: Number(amount) })}
+      >
+        Withdraw
       </button>
     </>
   );
